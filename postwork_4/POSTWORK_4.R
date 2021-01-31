0f67d478ddc27e75e4b3b47bbc7cc24bb8a8041b
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2021-01-31 14:30:41 -0600

    CREATE: Postwork 6, UPDATE: Postwork 4, sólo encabezado

--- postwork_4/POSTWORK_4.R
+++ postwork_4/POSTWORK_4.R
@@ -1,26 +1,48 @@
-#Ahora investigarás la dependencia o independencia del número de goles anotados 
-#por el equipo de casa y el número de goles anotados por el equipo visitante mediante 
-#un procedimiento denominado bootstrap, revisa bibliografía en internet para que 
+#Ahora investigar?s la dependencia o independencia del n?mero de goles anotados 
+#por el equipo de casa y el n?mero de goles anotados por el equipo visitante mediante 
+#un procedimiento denominado bootstrap, revisa bibliograf?a en internet para que 
 #tengas nociones de este desarrollo.
 
 suppressMessages(suppressWarnings(library(dplyr)))
+
+##### 
+# FAVOR DE EJECUTAR EN CASO DE NO HABER GENERADO EL DF EN EL CASO ANTERIOR.
+
 setwd("C:/Users/HP/Desktop/CURSO BEDU FASE 2/archivos") # Depende del usuario
 getwd()
-tem2019_2020 <- read.csv("tem2019_2020.csv")
-tem2018_2019 <- read.csv("tem2018_2019.csv")
-tem2017_2018 <- read.csv("tem2017_2018.csv")
-tem2020<-select(tem2019_2020, Date,HomeTeam,AwayTeam,FTHG,FTAG,FTR)
-tem2019<-select(tem2018_2019, Date,HomeTeam,AwayTeam,FTHG,FTAG,FTR)
-tem2018<-select(tem2017_2018, Date,HomeTeam,AwayTeam,FTHG,FTAG,FTR)
-df2020<-data.frame(tem2020)
-df2019<-data.frame(tem2019)
-df2018<-data.frame(tem2018)
-ligaespañola<-rbind(df2020,df2019,df2018)
-LIGAESP <- mutate(ligaespañola, Date = as.Date(Date, "%d/%m/%y"))
-LIGAESP
+
+# Descarga los datos de la temporada 2019/2020
+
+url1 <- "https://www.football-data.co.uk/mmz4281/1718/SP1.csv"
+url2 <- "https://www.football-data.co.uk/mmz4281/1819/SP1.csv"
+url3 <- "https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
+
+download.file(url = url1, destfile = "Temporada2017-2018.csv", mode = "wb")
+download.file(url = url2, destfile = "Temporada2018-2019.csv", mode = "wb")
+download.file(url = url3, destfile = "Temporada2019-2020.csv", mode = "wb")
+
+datos2017 <- read.csv("Temporada2017-2018.csv")
+datos2018 <- read.csv("Temporada2018-2019.csv")
+datos2019 <- read.csv("Temporada2019-2020.csv")
+
+# Guardarlos en las variables, asÃ­ como seleccionar 
+
+datos2017 <- select(datos2017, Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR)
+datos2017 <- mutate(datos2017, Date = as.Date(Date, "%d/%m/%y"))
+
+datos2018 <- select(datos2018, Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR)
+datos2018 <- mutate(datos2018, Date = as.Date(Date, "%d/%m/%y"))
+
+datos2019 <- select(datos2019, Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR)
+datos2019 <- mutate(datos2019, Date = as.Date(Date, "%d/%m/%y"))
+
+LIGAESP <- rbind(datos2017, datos2018, datos2019)
+
+#####
+
 #1.-Ya hemos estimado las probabilidades conjuntas de que el equipo de casa anote X=x 
 #goles (x=0,1,... ,8), y el equipo visitante anote Y=y goles (y=0,1,... ,6), 
-#en un partido. Obtén una tabla de cocientes al dividir estas probabilidades 
+#en un partido. Obt?n una tabla de cocientes al dividir estas probabilidades 
 #conjuntas por el producto de las probabilidades marginales correspondientes.
 casaanote<-table(LIGAESP$FTHG)
 x<-prop.table(casaanote)
@@ -44,10 +66,10 @@ str(co)
 coci<-data.frame(co)
 coci
 
-#2.-Mediante un procedimiento de boostrap, obtén más cocientes similares a los 
+#2.-Mediante un procedimiento de boostrap, obt?n m?s cocientes similares a los 
 #obtenidos en la tabla del punto anterior. Esto para tener una idea de las distribuciones
-#de la cual vienen los cocientes en la tabla anterior. Menciona en cuáles casos le parece
-#razonable suponer que los cocientes de la tabla en el punto 1, son iguales a 1 (en tal caso tendríamos independencia de las variables aleatorias X y Y).
+#de la cual vienen los cocientes en la tabla anterior. Menciona en cu?les casos le parece
+#razonable suponer que los cocientes de la tabla en el punto 1, son iguales a 1 (en tal caso tendr?amos independencia de las variables aleatorias X y Y).
 library(rsample)
 #install.packages("rsample")
 coci2<-bootstraps(coci, times = 63)
@@ -62,7 +84,7 @@ coci<-sort(coci$co)
 (df2 <- cbind(coci,cociboostrap))
 (names(df2) <- c("valores", "cocientes", "bootstraps"))
 
-(df2 <- melt(df2)) # función del paquete reshape2
+(df2 <- melt(df2)) # funci?n del paquete reshape2
 df2
 
 # Las frecuencias relativas son muy parecidas a las probabilidades.
---
+++ postwork_6/postwork_6.R
@@ -0,0 +1,48 @@
+# Postwork 6
+
+install.packages("lubridate")
+library(lubridate) # para función ymd()
+
+url <- "https://github.com/beduExpert/Programacion-con-R-Santander/raw/master/Sesion-06/Postwork/match.data.csv"
+download.file(url = url, destfile = "match.data.csv", mode = "wb")
+
+# Almacenando data
+match.data <- read.csv("match.data.csv")
+head(match.data)
+summary(match.data)
+
+# Convirtiendo date a tipo fecha
+match.data <- mutate(match.data, date = as.Date(ymd(date)))
+head(match.data)
+summary(match.data)
+
+# Nueva columna que contenga la suma de goles por partido
+match.data <- mutate(match.data, sumagoles = as.integer(home.score + away.score))
+head(match.data); tail(match.data)
+
+# Promedio de la suma de goles
+round(mean(match.data$sumagoles),digits = 3)
+
+# Promedio por mes de la suma de goles
+match.data <- select(match.data, date, sumagoles)
+match.data <- mutate(match.data, month = month(match.data$date), year = year(match.data$date))
+match.data <- select(match.data, month, year, sumagoles)
+str(match.data)
+
+# Acumulando
+prom.match <- aggregate(sumagoles ~ month+year, match.data, mean)
+prom.match[1:96, ]
+
+yr <- prom.match$year[1]
+mo <- prom.match$month[1]
+
+# para acomodar en tabla
+timeserie <- ts(prom.match$sumagoles, start = c(yr,mo), freq = 12)
+class(timeserie)
+
+plot(timeserie, type = "o", ylab = "Goles", xlab = "Tiempo", main = "Promedio de goles desde 2010")
+
+# Crea la serie de tiempo del promedio por mes de la suma de goles hasta diciembre de 2019.
+
+
+
